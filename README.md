2fe1a6a9de2b9c6e5f837cc168bf34c71bb1a61f
ankushfs <ankush.patil@playyipli.com>
2023-03-14 13:38:40 +0530

    #MODIFIED transfer to @kreedalabs #FIXED console messages #ADDED cleanup only if clone success

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 Takes care of initialising an [express-on-steroids](https://github.com/Kreeda-Studios/express-on-steroids) project.
 
 Just do
-> npx create-eos-app test_api
+> npx @kreedalabs/create-eos-app test_api
 
 and a new project directory `test_api` will be created containing all the necessary code for an express-on-steroids package.
 
 
 * This script will also take care of install npm dependencies it requires.
+
--- app.js
+++ app.js
@@ -6,80 +6,92 @@ const path = require("path");
 const child_process = require("child_process");
 const yargs = require("yargs")
 
+let IS_CLONE_SUCCESS = false;
+
 /**
  * @type {[]}
  */
-const args = yargs.argv
-// const gitUrl = "https://github.com/facebook/create-react-app.git";
+const args = yargs.argv;
 const gitUrl = "https://github.com/Kreeda-Studios/express-on-steroids.git";
 /**
  * @type {String}
  */
 const targetProjectName = yargs.argv._[0] || "express-on-steroids";
-console.log(process.argv);
-console.log(yargs.argv)
-console.log("tpn", targetProjectName)
+
 if (targetProjectName.trim() === "*") {
   console.log(targetProjectName, " is an invalid project name. Aborting...");
   process.exit();
 }
 
 function cleanup() {
-  console.log("cleaning up...");
+  console.log();
+  console.log("=========== cleaning up... ===========");
   try {
     console.log("removing: ", path.join(".", targetProjectName));
+
     fs.rmSync(path.join(".", targetProjectName), {
       recursive: true,
       force: true,
     });
+    console.log("cleanup success");
   } catch (error) {
     console.log("cleanup failed. :-(");
   }
+  console.log("======================");
 }
 
 function handleSigint() {
-  cleanup();
+  if (IS_CLONE_SUCCESS) {
+    cleanup();
+  }
+  console.log();
   console.log("exiting");
+  console.log();
   process.exit();
 }
 
 function handleError(e) {
-  console.error("eroor", e);
-  cleanup();
-  process.exit();
+  console.error("error: ", e);
+  if (IS_CLONE_SUCCESS) {
+    cleanup();
+  }
+  process.exit(1);
 }
 
 function npmInstall() {
-  console.log("installing dependencies with npm");
+  console.log();
+  console.log("=========== installing dependencies with npm... ===========");
   try {
     const result = child_process.execSync(
       `cd ${path.join(".", targetProjectName)} && npm install`
     );
-    console.log("npm deps installation result: ", result.toString());
+    console.log(result.toString());
+    console.log("npm dependencies installed");
   } catch (error) {
     console.log("failed to install deps");
   }
+  console.log("===================");
 }
 
 function cloneRepo() {
-  console.log()
-  console.log("cloning eos");
+  console.log();
+  console.log("=========== cloning eos... ===========");
   const result = child_process.execSync(
     `git clone ${gitUrl} ${targetProjectName}`
-    );
+  );
   console.log("clone success");
+  IS_CLONE_SUCCESS = true;
   console.log(result.toString());
-  console.log()
+  console.log("====================");
 }
 
 function renameProject() {
-  console.log("removing package-lock");
+  console.log();
+  console.log("=========== adding final touches... ===========");
   try {
     fs.rmSync(path.join(".", targetProjectName, "package-lock.json"));
-  } catch (error) {
-    console.log("failed to remove package-lock.json");
-  }
-  console.log("reading package.json");
+  } catch (error) {}
+  // console.log("reading package.json");
   try {
     const json = JSON.parse(
       fs.readFileSync(path.join(".", targetProjectName, "package.json"))
@@ -92,18 +104,41 @@ function renameProject() {
       path.join(".", targetProjectName, "package.json"),
       JSON.stringify(json)
     );
-    console.log("renamed project");
+    console.log("done");
   } catch (error) {
-    console.log("failed to rename project");
+    console.log("failed to add final touches. :-(");
   }
+  console.log("=========================");
 }
 function openWithCode() {
-  console.log("opening with code")
+  console.log("opening with code");
   try {
     child_process.execSync(`code ${path.join(".", targetProjectName)}`);
   } catch (error) {
-    console.error("failed to open project with code...")
+    console.error("failed to open project with code...");
   }
+}
+
+function printKreedaAsciiArt() {
+  console.log();
+  console.log(`  _   __  __ __                 _         _       _          `);
+  console.log(
+    ` | | / / / / \\ \\               | |       | |     | |         `
+  );
+  console.log(` | |/ / | |_ _| |  ___  ___  __| | __ _  | | __ _| |__  ___  `);
+  console.log(
+    ` |    \\/ /| '__\\ \\/ _ \\/ _ \\/ _\` |/ _\` | | |/ _\` | '_ \\/ __| `
+  );
+  console.log(
+    ` | |\\  \\ \\| |  / /  __/  __/ (_| | (_| | | | (_| | |_) \\__ \\ `
+  );
+  console.log(
+    ` \\_| \\_/| |_| | | \\___|\\___|\\__,_|\\__,_| |_|\\__,_|_.__/|___/ `
+  );
+  console.log(
+    `         \\_\\ /_/                                             `
+  );
+  console.log(`                                                             `);
 }
 
 process.on("SIGINT", handleSigint);
@@ -111,14 +146,27 @@ process.on("uncaughtException", handleError);
 
 // console.log("yargs", args)
 // console.log("args ", process.argv);
+console.log();
+console.log("Hello, from Kreeda Labs Team.");
+
 cloneRepo();
 npmInstall();
 renameProject();
-console.log(
-  "check out express-on-steroids: https://github.com/Kreeda-Studios/express-on-steroids"
-);
 if (args["code"]) {
   console.log("opening with code");
   openWithCode();
 }
+
+printKreedaAsciiArt();
+
+console.log();
+console.log(
+  "check out express-on-steroids: https://github.com/Kreeda-Studios/express-on-steroids"
+);
+console.log(
+  `run "cd ${targetProjectName}" and "npm start" to see express on steroids in action.`
+);
+console.log("Bye Bye...");
+console.log();
+
 
